be5faff6f023c29d985bf035872d4a85f0636778
kosuke <user@email.com>
2020-11-08 15:04:06 +0000

    5.5.3 StrongParameters 終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+ 
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params_user)
+    if @user.save
+    else
+      render :new
+    end
+  end
+  
+  private
+    def params_user
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
